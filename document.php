32b865951c7a0e66ea8ff583b11bc35c26047ad7
Thijs Miedema <t.miedema@live.nl>
2017-03-22 11:36:20 +0100

    Added some figure stuff and changed documentation to reflect it

--- document.php
+++ document.php
@@ -5,20 +5,20 @@ require_once (__DIR__."/module.php");
 
 class Document extends MarTeXmodule {
     public function registerCommands() {
-        return array("section", "subsection", "subsubsection", "textbf", "textit", "underline", "hline", "smallcaps", "ref", "refpass", "define", "newline", "paragraph", "title", "href"); 
+        return array("section", "subsection", "subsubsection", "textbf", "textit", "underline", "hline", "smallcaps", "ref", "refpass", "define", "newline", "paragraph", "title", "href", "par","color", "colour"); 
     }
     
     public function handleCommand($command, $argument) {
 
         switch($command) {
             case "section":
-                $argument = $this->valisaniArgument($argument, 1, "String/nowhitespace");
-                return "<h2>".$argument."</h2>";
+                $argument = $this->valisaniArgument($argument, 1, "String");
+                return "<h2>".$argument."</h2>";     
             case "subsection":
-                $argument = $this->valisaniArgument($argument, 1, "String/nowhitespace");
+                $argument = $this->valisaniArgument($argument, 1, "String");
                 return "<h3>".$argument."</h3>";
             case "subsubsection":
-                $argument = $this->valisaniArgument($argument, 1, "String/nowhitespace");
+                $argument = $this->valisaniArgument($argument, 1, "String");
                 return "<h4>".$argument."</h4>";
             case "textbf":
                 $argument = $this->valisaniArgument($argument, 1, "String");
@@ -69,7 +69,97 @@ class Document extends MarTeXmodule {
                 return "<br>"; 
             case "href":
                 $argument = $this->valisaniArgument($argument, 2, array("String", "String"));
-                return "<a href='".$argument[0]."'>".$argument[1]."</a>";              
+                return "<a href='".$argument[0]."'>".$argument[1]."</a>";
+            case "par":
+                return "<br><br>";
+            case "color":
+            case "colour":
+                if (!is_array($argument) or count($argument) < 2) {
+                    $this->raiseError("Invalid color command.");
+                    return "";
+                }
+                $coltype = "";
+                $coldef = "";
+                $txt = "";
+                if (count($argument) == 2) {
+                    $coldef = $argument[0];
+                    $txt = $argument[1];
+                    if ($argument[0][0] == '#') {
+                        $coltype = "hex";
+                    }
+                    else if (substr_count($argument[0], ',') == 2) {
+                        $coltype = "rgb";
+                    }
+                    else if (substr_count($argument[0], ',') == 3) {
+                        $coltype = "rgba";
+                    }
+                    else {
+                        $this->raiseError("Unable to guess colour type.");
+                        return "";
+                    }
+                }
+                else if (count($argument == 3)) {
+                    $coltype = $argument[0];
+                    $coldef = $argument[1];
+                    $txt = $argument[2];
+                }
+                else {
+                    $this->raiseError("Invalid number of arguments.");
+                    return "";
+                }
+                
+                switch($coltype) {
+                    case "hex":
+                        return "<span style='color:#" . 
+                            preg_replace("/[^a-zA-Z0-9]+/", "", $coldef) . 
+                             "'>".$txt."</span>";
+                    case "rgb":
+                        $bins = explode(',' , $coldef);
+                        if (count($bins) != 3) {
+                            $this->raiseError("Invalid rgb colour definition.");
+                            return "";
+                        }
+                        return "<span style='color:rgb(" . 
+                            preg_replace("/[^a-zA-Z0-9]+/", "", $bins[0]) . "," .
+                            preg_replace("/[^a-zA-Z0-9]+/", "", $bins[1]) . "," .
+                            preg_replace("/[^a-zA-Z0-9]+/", "", $bins[2]) . 
+                             ")'>".$txt."</span>";
+                    case "rgba":
+                        $bins = explode(',', $coldef);
+                        if (count($bins) != 4) {
+                            $this->raiseError("Invalid rgb colour definition.");
+                            return "";
+                        }
+                        return "<span style='color:rgba(" . 
+                            preg_replace("/[^a-zA-Z0-9]+/", "", $bins[0]) . "," .
+                            preg_replace("/[^a-zA-Z0-9]+/", "", $bins[1]) . "," .
+                            preg_replace("/[^a-zA-Z0-9]+/", "", $bins[2]) . "," .
+                            preg_replace("/[^a-zA-Z0-9.]+/", "", $bins[3]) .
+                             ")'>".$txt."</span>";
+                    case "hsl":
+                        $bins = explode(',', $coldef);
+                        if (count($bins) != 3) {
+                            $this->raiseError("Invalid rgb colour definition.");
+                            return "";
+                        }
+                        return "<span style='color:hsl(" . 
+                            preg_replace("/[^a-zA-Z0-9]+/", "", $bins[0]) . "," .
+                            preg_replace("/[^a-zA-Z0-9]+/", "", $bins[1]) . "%," .
+                            preg_replace("/[^a-zA-Z0-9]+/", "", $bins[2]) . "%" .
+                             ")'>".$txt."</span>";
+                    case "hsla":
+                       $bins = explode(',', $coldef);
+                        if (count($bins) != 4) {
+                            $this->raiseError("Invalid hsla colour definition.");
+                            return "";
+                        }
+                        return "<span style='color:hsla(" . 
+                            preg_replace("/[^a-zA-Z0-9]+/", "", $bins[0]) . "," .
+                            preg_replace("/[^a-zA-Z0-9]+/", "", $bins[1]) . "%," .
+                            preg_replace("/[^a-zA-Z0-9]+/", "", $bins[2]) . "%," .
+                            preg_replace("/[^a-zA-Z0-9.]+/", "", $bins[3]) . 
+                             ")'>".$txt."</span>";
+                }
         }
     }
     

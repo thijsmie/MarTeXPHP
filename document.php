3dbae1470575157bfb572ba13fa11a0dd60e936c
ARand0mCoder <d.leijnse@gmail.com>
2017-06-16 17:10:32 +0200

    Center environment toegevoegd

--- document.php
+++ document.php
@@ -72,6 +72,15 @@ class Document extends MarTeXmodule {
                 return "<a href='".$argument[0]."'>".$argument[1]."</a>";
             case "par":
                 return "<br><br>";
+			case "begin":
+				$argument = $this->valisaniArgument($argument, 1, "String/nowhitespace");
+				if ($argument == "center"):
+					return "<p style=\"text-align:center;\">";
+				return;
+			case "end":
+				$argument = $this->valisaniArgument($argument, 1, "String/nowhitespace");
+				if ($argument == "center"):
+					return "</p>";
             case "color":
             case "colour":
                 if (!is_array($argument) or count($argument) < 2) {
